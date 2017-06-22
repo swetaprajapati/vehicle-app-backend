fbb1e4a6479086e4b235b203b148948177ba00ac
sweta <youremail@example.com>
2017-06-22 17:20:48 +0530

    make it better

---
+++ routes/index.js
@@ -0,0 +1,44 @@
+var express = require('express');
+var router = express.Router();
+
+var URL="mongodb://sweta1:123456@ds045882.mlab.com:45882/bustrackingapp"
+
+const db=require('monk')(URL)
+
+const docs=db.get('dbs')
+
+
+/* GET home page. */
+router.get('/welcome', function(req, res, next) {
+  //res.render('index', { title: 'Express' });
+
+    docs.find( {} ,function (err,docs) {
+      if(err) console.log(err)
+      else res.json(docs[1]);
+
+  });
+
+
+
+
+});
+
+router.get('/welcomeppl', function(req, res, next) {
+
+    docs.insert({"name": "chd", "place": "awesome"}, function (err, docs) {
+        if (err) console.log(err)
+
+        else res.send("success");
+
+    });
+
+});
+
+
+
+
+
+router.get('/wel', function(req, res, next) {
+    res.send('sweta is working ');
+});
+module.exports = router;
