77e44f3bd0f4393d0a0cc2a8833af6e4568f35ae
sweta <youremail@example.com>
2017-06-23 17:48:25 +0530

    23rd june

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,12 @@
 var express = require('express');
 var router = express.Router();
 
-var URL="mongodb://sweta1:123456@ds045882.mlab.com:45882/bustrackingapp"
+var URL="mongodb://sweta1:123456@ds045882.mlab.com:45882/bustrackingapp";
 
-const db=require('monk')(URL)
+const db=require('monk')(URL);
 
-const docs=db.get('dbs')
+const docs=db.get('dbs');
+
 
 
 /* GET home page. */
@@ -13,7 +14,7 @@ router.get('/welcome', function(req, res, next) {
   //res.render('index', { title: 'Express' });
 
     docs.find( {} ,function (err,docs) {
-      if(err) console.log(err)
+      if(err) console.log(err);
       else res.json(docs[1]);
 
   });
@@ -25,14 +26,35 @@ router.get('/welcome', function(req, res, next) {
 
 router.get('/welcomeppl', function(req, res, next) {
 
-    docs.insert({"name": "chd", "place": "awesome"}, function (err, docs) {
+
+   docs.insert({"name": "chd", "place": "awesome"}, function (err, docs) {
         if (err) console.log(err)
 
-        else res.send("Welcome to heroku");
+      else res.send("Welcome to heroku");
 
     });
 
 });
+
+
+router.get('/push',function (req,res,next) {
+
+    docs.update ({"emailid":"swetaprajapati1997@gmail.com",
+    "id":"5e438"},{$push : {"group":{"name":"user3"}}
+    },function (err,docs) {
+        if (err) console.log(err)
+
+        else res.send("Your update is successful");
+
+
+    });
+
+});
+
+
+
+
+
 
 
 
